f677215c5faf0156b46c004bef19e006794c8a39
btdrizzle <briandrake05@gmail.com>
2019-01-25 00:33:15 -0600

    the weather station can talk to the database!

---
+++ models/Weather.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+var WeatherSchema = new Schema({
+  // `title` is required and of type String
+  station: {
+    type: String,
+  },
+  // `link` is required and of type String
+  name: {
+      type:String
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+    },
+  synopsis: {
+    type: String,
+
+    },
+  thumbnail: {
+    type: String,
+
+    },
+  link: {
+    type: String,
+  }
+});
+
+var Weather = mongoose.model("Weather", WeatherSchema);
+
+module.exports = Weather;
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
+const db = require("../models");
 
+router.post("/weather", (req, res) => {
+    const info = req.body;
+    console.log(info);
+
+    db.Weather.create({name: info.name })
+        .then((data) => {
+            res.status(200).json(data);
+        }).catch(err => console.log(err));
+});
 
 module.exports = router;
