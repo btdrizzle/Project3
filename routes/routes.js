c686170d81f301742cd2fcb92d6bc2811f0cf3e9
btdrizzle <briandrake05@gmail.com>
2019-01-26 09:30:26 -0600

    deployed to heroku

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ router.post("/weather", (req, res) => {
     const info = req.body;
     console.log(info);
 
-    db.Weather.create({name: info.name })
+    db.Weather.create({ name: info.name })
         .then((data) => {
             res.status(200).json(data);
         }).catch(err => console.log(err));
