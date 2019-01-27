8701c1260236c073710da38c0362b0f36bd6de32
btdrizzle <briandrake05@gmail.com>
2019-01-26 22:23:37 -0600

    middle of creating react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/books" component={Books} />
+          <Route path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+  
       </div>
+    </Router>
     );
   }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
+const axios = require("axios");
 const db = require("../models");
 require("dotenv").config();
+
 
 router.post("/weather/:station", (req, res) => {
     const { station } = req.params;
@@ -15,8 +17,22 @@ router.post("/weather/:station", (req, res) => {
         }).catch(err => res.json(err));
 });
 
-/* router.get("/weather/:station", (req,res) => {
+router.get("/weather/:station", (req, res) => {
+    const { station } = req.params;
+    db.Station.findOne({ _id: station })
+        .then((data) => {
+            db.Weather.find({ station: data._id })
+                .then((weatherData) => {
+                    res.status(200).json(weatherData);
+                }).catch(err => console.log(err));
+        }).catch(err => res.json(err));
+});
 
-}) */
+router.get("/weather/weatherbit/", (req,res) => {
+    const baseURL = "http://http://api.weatherbit.io/v2.0/history/daily?";
+    const KEY = process.env.API_Key;
+    const body = req.body;
+
+})
 
 module.exports = router;
