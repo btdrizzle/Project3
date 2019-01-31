196c4f9cfd28574c18359378810d91efeb9d5e69
btdrizzle <briandrake05@gmail.com>
2019-01-31 15:28:36 -0600

    small comparison chart changes.  grammar and colors

--- client/src/components/ComparisonChart.js
+++ client/src/components/ComparisonChart.js
@@ -5,15 +5,15 @@ import "./style.css";
 function ComparisonChart(props) {
     return(
         <div>
-        <h3 className="text-center">High and Low Temp Today so Far Compared to {props.location}</h3>
+        <h3 className="text-center">High and Low Temp So Far Today Compared to {props.location}</h3>
         <BarChart width={900} height={400} data={props.data} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
             <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
             <XAxis dataKey="name"/>
             <YAxis label={{ value: "Temp F", angle: -90, position: 'insideLeft'}}/>
             <Tooltip />
             <Legend verticalAlign="bottom" height={36}/>
-            <Bar dataKey="highTemp" fill="#8884d8" />
-            <Bar dataKey="lowTemp" fill="#82ca9d" />
+            <Bar dataKey="highTemp" fill="#ce2029" />
+            <Bar dataKey="lowTemp" fill="#fcc200" />
             </BarChart>
         </div>
     )
